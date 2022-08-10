7e6aa20da72f4fe35493fba2b21c7749b5f44880
刘星 <753880957@qq.com>
2022-08-10 17:19:58 +0800

    优化order 添加dispatch

--- page_objectes/dispatch_page.py
+++ page_objectes/dispatch_page.py
@@ -6,6 +6,15 @@ from page_objectes.base_page import BasePage
 
 
 class DispatchPage(BasePage):
+    __INPUT_DRIVER_NAME
+    __INPUT_DRIVER_PHONE
+    __INPUT_VEHICLE
+    __INPUT_DATA
+    __BTN_ADVANCE
+    __INPUT_ADVANCE
+    __INPUT_DELIVER
+
     def input_dispatch(self):
-        self.do_send_keys("100","//*[@data-testid='oil-payment-input']")
+
+        self.do_send_keys("100",By.XPATH,"//*[@data-testid='oil-payment-input']")
         sleep(10)
