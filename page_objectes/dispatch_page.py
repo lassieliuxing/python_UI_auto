5a852f9a1103b5c72ca5063d9e24a18532fe3856
刘星 <753880957@qq.com>
2022-08-10 17:32:41 +0800

    添加调度元素定位

--- page_objectes/dispatch_page.py
+++ page_objectes/dispatch_page.py
@@ -6,13 +6,14 @@ from page_objectes.base_page import BasePage
 
 
 class DispatchPage(BasePage):
-    __INPUT_DRIVER_NAME
-    __INPUT_DRIVER_PHONE
-    __INPUT_VEHICLE
-    __INPUT_DATA
-    __BTN_ADVANCE
-    __INPUT_ADVANCE
-    __INPUT_DELIVER
+    __INPUT_DRIVER_NAME=(By.XPATH,"//*[@id='rc_select_12']")
+    __INPUT_DRIVER_PHONE=(By.XPATH,"//*[@data-testid='dispatch-waybill-drivers-phone-select']")
+    __INPUT_VEHICLE=(By.XPATH,"//*[@id='rc_select_13']")
+    __INPUT_DATA=(By.XPATH,"//*[@id='planLoadingAt']")
+    __BTN_ADVANCE=(By.XPATH,"//button[@advance-checked='advance-checked']")
+    __INPUT_ADVANCE_CASH=(By.XPATH,"//input[@data-testid='oil-payment-input']")
+    __INPUT_ADVANCE_OILD=(By.XPATH,"//input[@data-testid='oil-amount-input']")
+    __INPUT_DELIVER=(By.XPATH,"")
 
     def input_dispatch(self):
 
