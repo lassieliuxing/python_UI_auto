ac5d2cb214bda91f8d7bbdbf7f6e224765cff1bd
刘星 <753880957@qq.com>
2022-08-11 15:48:08 +0800

    添加调度元素定位

--- test_begain/DispatchTest.py
+++ test_begain/DispatchTest.py
@@ -5,10 +5,9 @@ class Test_Dispatch:
 
         def setup_class(self):
             self.home = LoginPage().login("13900000000", "1111111l")
-
-        def teardown_class(self):
-            self.home.do_quit()
+        #
+        # def teardown_class(self):
+        #     self.home.do_quit()
         def test_dispatch(self):
-            self.home.goto_dispatch()\
-                .diapstch_order()\
-                .input_dispatch()
+            self.home.goto_dispatch()  \
+            .diapstch_order() .input_dispatch_with_deliver()
